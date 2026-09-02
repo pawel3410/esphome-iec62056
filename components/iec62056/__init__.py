efb5a8161f3c542b069b6ee5d827ee89cdf3c65b
pawel3410 <pawel3410@gmail.com>
2026-09-02 12:18:10 +0200

    Update regex for OBIS code format validation
    Refactor regex for OBIS code validation to allow optional prefix and improve matching.

--- components/iec62056/__init__.py
+++ components/iec62056/__init__.py
@@ -46,7 +46,9 @@ def validate_obis(value):
     # 0-0:1.0.0*102
     # C.P.5
     # Allow any uppercase alphanumeric character in the OBIS code
-    rx = r"(\d+-\d+\:){,1}[\dA-Z]+\.[\dA-Z]+(.[\dA-Z]+){,1}(\*\d+){,1}"
+#    rx = r"(\d+-\d+\:){,1}[\dA-Z]+\.[\dA-Z]+(.[\dA-Z]+){,1}(\*\d+){,1}"
+        # dodaje "107" "1007" 
+    rx = r"(\d+-\d+:)?[\dA-Z]+(\.[\dA-Z]+){0,2}(\*\d+)?"
 
     m = re.fullmatch(rx, value)
     if m is None:
